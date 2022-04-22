bb5eccd4b953effa4ee0ff54f3c42f0df7817a73
Pedro Lyra <79703395+PedroLyra22@users.noreply.github.com>
2022-04-22 17:03:54 -0300

    Update scriptPostgreSQL.sql

--- pset1/postgreSQL/scriptPostgreSQL.sql
+++ pset1/postgreSQL/scriptPostgreSQL.sql
@@ -204,6 +204,23 @@ VALUES( 'Novosbenefícios', 30, 'Mauá', 4);
 /*Inserção dos valores na tabela projeto.*/
 
 
+INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
+VALUES( '33344555587', 'Alícia', 'F', '1986-04-05', 'Filha');
+INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
+VALUES( '33344555587', 'Tiago', 'M', '1983-10-25', 'Filho');
+INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
+VALUES( '33344555587', 'Janaína', 'F', '1958-05-03', 'Esposa');
+INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
+VALUES( '98765432168', 'Antonio', 'M', '1942-02-28', 'Marido');
+INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
+VALUES( '12345678966', 'Michael', 'M', '1988-01-04', 'Filho');
+INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
+VALUES( '12345678966', 'Alícia', 'F', '1988-12-30', 'Filha');
+INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
+VALUES( '12345678966', 'Elizabeth', 'F', '1967-05-05', 'Esposa');
+/*Inserção dos valores na tabela dependente.*/
+
+
 INSERT INTO elmasri.trabalha_em( cpf_funcionario, numero_projeto, horas)
 VALUES( '12345678966', 1, '32.5');
 INSERT INTO elmasri.trabalha_em( cpf_funcionario, numero_projeto, horas)
@@ -237,20 +254,3 @@ VALUES( '98765432168', 20, '15.0');
 INSERT INTO elmasri.trabalha_em( cpf_funcionario, numero_projeto, horas)
 VALUES( '88866555576', 20, '0');
 /*Inserção dos valores na tabela trabalha_em.*/
-
-
-INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
-VALUES( '33344555587', 'Alícia', 'F', '1986-04-05', 'Filha');
-INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
-VALUES( '33344555587', 'Tiago', 'M', '1983-10-25', 'Filho');
-INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
-VALUES( '33344555587', 'Janaína', 'F', '1958-05-03', 'Esposa');
-INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
-VALUES( '98765432168', 'Antonio', 'M', '1942-02-28', 'Marido');
-INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
-VALUES( '12345678966', 'Michael', 'M', '1988-01-04', 'Filho');
-INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
-VALUES( '12345678966', 'Alícia', 'F', '1988-12-30', 'Filha');
-INSERT INTO elmasri.dependente( cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
-VALUES( '12345678966', 'Elizabeth', 'F', '1967-05-05', 'Esposa');
-/*Inserção dos valores na tabela dependente.*/
